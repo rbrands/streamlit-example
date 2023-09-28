103bc0d8cb9c2b676722ac5a10e2625abd27d8d4
Robert Brands <mail@robert-brands.com>
2023-09-28 19:29:01 +0000

    Multipage

---
+++ pages/About.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+"""
+# About
+
+This is a demo application to show:
+- How to host a Streamlit-App easily in Azure via App Service
+- Usage of very simple authentication.
+- Usage of Azure services e.g. Blob-storage with the Python SDK.
+- Streamlit File Uploader to blob storage
+- Python/Streamlit development mit GitHub Codespaces
+
+See GitHub repo for details: https://github.com/rbrands/streamlit-example
+"""
